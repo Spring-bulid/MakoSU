tiann/KernelSU

manager: replace HanziToPinyin with icu transliteration

manager/app/src/main/java/me/weishu/kernelsu/ui/util/PinyinUtil.kt
@@ -1,0 +1,14 @@
+package me.weishu.kernelsu.ui.util
+
+import android.icu.text.Transliterator
+
+object PinyinUtil {
+
+    private val transliterator: Transliterator by lazy {
+        Transliterator.getInstance("Han-Latin; Latin-ASCII; Lower")
+    }
+
+    @Synchronized
+    fun toPinyin(input: String): String =
+        transliterator.transliterate(input).replace(" ", "")
+}

manager/app/src/main/java/me/weishu/kernelsu/ui/viewmodel/ModuleRepoViewModel.kt
@@ -20,6 +20,7 @@ import me.weishu.kernelsu.ksuApp
 import me.weishu.kernelsu.ui.component.SearchStatus
 import me.weishu.kernelsu.ui.screen.modulerepo.ModuleRepoUiState
 import me.weishu.kernelsu.ui.screen.modulerepo.RepoSort
+import me.weishu.kernelsu.ui.util.PinyinUtil
 import me.weishu.kernelsu.ui.util.isNetworkAvailable
 import java.text.Collator
 import java.util.Locale
@@ -75,8 +76,7 @@ class ModuleRepoViewModel(
                     it.moduleName.contains(text, true) ||
                     it.authors.contains(text, true) ||
                     it.summary.contains(text, true) ||
-                    me.weishu.kernelsu.ui.util.HanziToPinyin.getInstance().toPinyinString(it.moduleName)
-                        .contains(text, true)
+                    PinyinUtil.toPinyin(it.moduleName).contains(text, true)
         }
     }
 

manager/app/src/main/java/me/weishu/kernelsu/ui/viewmodel/ModuleViewModel.kt
@@ -35,6 +35,7 @@ import me.weishu.kernelsu.ui.screen.module.ModuleConfirmDialogState
 import me.weishu.kernelsu.ui.screen.module.ModuleConfirmRequest
 import me.weishu.kernelsu.ui.screen.module.ModuleEffect
 import me.weishu.kernelsu.ui.screen.module.ModuleUiState
+import me.weishu.kernelsu.ui.util.PinyinUtil
 import me.weishu.kernelsu.ui.util.hasMagisk
 import me.weishu.kernelsu.ui.util.module.fetchModuleDetail
 import me.weishu.kernelsu.ui.util.module.fetchReleaseDescriptionHtml
@@ -149,8 +150,7 @@ class ModuleViewModel(
         return modules.filter {
             it.id.contains(text, true) || it.name.contains(text, true) ||
                     it.description.contains(text, true) || it.author.contains(text, true) ||
-                    me.weishu.kernelsu.ui.util.HanziToPinyin.getInstance().toPinyinString(it.name)
-                        .contains(text, true)
+                    PinyinUtil.toPinyin(it.name).contains(text, true)
         }
     }
 

manager/app/src/main/java/me/weishu/kernelsu/ui/viewmodel/SuperUserViewModel.kt
@@ -23,7 +23,7 @@ import me.weishu.kernelsu.ksuApp
 import me.weishu.kernelsu.ui.component.SearchStatus
 import me.weishu.kernelsu.ui.screen.superuser.GroupedApps
 import me.weishu.kernelsu.ui.screen.superuser.SuperUserUiState
-import me.weishu.kernelsu.ui.util.HanziToPinyin
+import me.weishu.kernelsu.ui.util.PinyinUtil
 import me.weishu.kernelsu.ui.util.ownerNameForUid
 import me.weishu.kernelsu.ui.util.pickPrimary
 import java.text.Collator
@@ -180,7 +180,7 @@ class SuperUserViewModel(
             val matchedPackageNames = group.apps.filter {
                 it.label.contains(text, true) ||
                         it.packageName.contains(text, true) ||
-                        HanziToPinyin.getInstance().toPinyinString(it.label).contains(text, true)
+                        PinyinUtil.toPinyin(it.label).contains(text, true)
             }.mapTo(linkedSetOf()) { it.packageName }
 
             if (matchedPackageNames.isEmpty()) {

Spring-bulid/MakoSU

manager/app/src/main/java/com/sukisu/ultra/ui/util/PinyinUtil.kt
@@ -1,0 +1,14 @@
+package com.sukisu.ultra.ui.util
+
+import android.icu.text.Transliterator
+
+object PinyinUtil {
+
+    private val transliterator: Transliterator by lazy {
+        Transliterator.getInstance("Han-Latin; Latin-ASCII; Lower")
+    }
+
+    @Synchronized
+    fun toPinyin(input: String): String =
+        transliterator.transliterate(input).replace(" ", "")
+}

manager/app/src/main/java/com/sukisu/ultra/ui/viewmodel/ModuleRepoViewModel.kt
@@ -20,6 +20,7 @@ import com.sukisu.ultra.ksuApp
 import com.sukisu.ultra.ui.component.SearchStatus
 import com.sukisu.ultra.ui.screen.modulerepo.ModuleRepoUiState
 import com.sukisu.ultra.ui.screen.modulerepo.RepoSort
+import com.sukisu.ultra.ui.util.PinyinUtil
 import com.sukisu.ultra.ui.util.isNetworkAvailable
 import java.text.Collator
 import java.util.Locale
@@ -74,8 +75,7 @@ class ModuleRepoViewModel(
                     it.moduleName.contains(text, true) ||
                     it.authors.contains(text, true) ||
                     it.summary.contains(text, true) ||
-                    com.sukisu.ultra.ui.util.HanziToPinyin.getInstance().toPinyinString(it.moduleName)
-                        .contains(text, true)
+                    PinyinUtil.toPinyin(it.moduleName).contains(text, true)
         }
     }
 

manager/app/src/main/java/com/sukisu/ultra/ui/viewmodel/ModuleViewModel.kt
@@ -41,6 +41,7 @@ import com.sukisu.ultra.ui.screen.module.ModuleConfirmDialogState
 import com.sukisu.ultra.ui.screen.module.ModuleConfirmRequest
 import com.sukisu.ultra.ui.screen.module.ModuleEffect
 import com.sukisu.ultra.ui.screen.module.ModuleUiState
+import com.sukisu.ultra.ui.util.PinyinUtil
 import com.sukisu.ultra.ui.util.hasMagisk
 import com.sukisu.ultra.ui.util.module.fetchModuleDetail
 import com.sukisu.ultra.ui.util.module.fetchReleaseDescriptionHtml
@@ -192,8 +193,7 @@ class ModuleViewModel(
         return modules.filter {
             it.id.contains(text, true) || it.name.contains(text, true) ||
                     it.description.contains(text, true) || it.author.contains(text, true) ||
-                    com.sukisu.ultra.ui.util.HanziToPinyin.getInstance().toPinyinString(it.name)
-                        .contains(text, true)
+                    PinyinUtil.toPinyin(it.name).contains(text, true)
         }
     }
 

manager/app/src/main/java/com/sukisu/ultra/ui/viewmodel/SuperUserViewModel.kt
@@ -23,7 +23,7 @@ import com.sukisu.ultra.ksuApp
 import com.sukisu.ultra.ui.component.SearchStatus
 import com.sukisu.ultra.ui.screen.superuser.GroupedApps
 import com.sukisu.ultra.ui.screen.superuser.SuperUserUiState
-import com.sukisu.ultra.ui.util.HanziToPinyin
+import com.sukisu.ultra.ui.util.PinyinUtil
 import com.sukisu.ultra.ui.util.ownerNameForUid
 import com.sukisu.ultra.ui.util.pickPrimary
 import java.text.Collator
@@ -189,7 +189,7 @@ class SuperUserViewModel(
             val matchedPackageNames = group.apps.filter {
                 it.label.contains(text, true) ||
                         it.packageName.contains(text, true) ||
-                        HanziToPinyin.getInstance().toPinyinString(it.label).contains(text, true)
+                        PinyinUtil.toPinyin(it.label).contains(text, true)
             }.mapTo(linkedSetOf()) { it.packageName }
 
             if (matchedPackageNames.isEmpty()) {